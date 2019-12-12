bab856ac05cabe317c2ed1f1eaf65cdb24c4e870
richieflicker <richardpeter07@gmail.com>
2019-12-12 12:07:04 +0530

    Derpfest Bringup

--- aosip_potter.mk
+++ aosip_potter.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common.mk)
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit from potter device
 $(call inherit-product, device/motorola/potter/device.mk)
@@ -29,7 +29,8 @@ $(call inherit-product, device/motorola/potter/device.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
-
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := potter
 PRODUCT_NAME := aosip_potter
