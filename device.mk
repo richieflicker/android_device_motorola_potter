ffa189b280ff27d23aca053cafa5d75f338fc263
ZJRDroid <priv.zjr@gmail.com>
2020-01-18 10:06:54 -0300

    potter: Display ARCore apps on Play Store

--- device.mk
+++ device.mk
@@ -323,7 +323,8 @@ PRODUCT_COPY_FILES += \
 # Qualcomm
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/android.hardware.camera.ar.xml:system/vendor/etc/permissions/android.hardware.camera.ar.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
